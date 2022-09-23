32a5433054d0105c610aa8c9f227f2df42ab7f31
Chris Steele <chris@me.com>
2022-09-23 16:06:10 -0500

    Added export data transform.

--- src/API/constants.js
+++ src/API/constants.js
@@ -1,8 +1,11 @@
 export const ids = {
-    X_API_TOKEN: "", 
-    SURVEY_ID: "",
-    DEFAULT_DIRECTORY: "",
-    MAILINGLIST_ID: ""
+    X_API_TOKEN: "S6Vip4gGnK5e42fPs6qHPLN1Qy3LCMi7MkFcZUKy",
+
+    SURVEY_ID: "SV_8GMa77xhDaue9zE",
+
+    DEFAULT_DIRECTORY: "POOL_28S50Seuo8J1WU8",
+
+    MAILINGLIST_ID: "CG_2tiikgnKRw7zUjS"
 }
 
 export let headers = new Headers()
--- src/Components/ExportData.js
+++ src/Components/ExportData.js
@@ -5,21 +5,34 @@ const ExportData = () => {
     
     const exportingData = async () =>{
 
-        //starting the response export
+        // get question meta data
+        const getQuestionsDataRequestOptions = {
+            method: 'GET',
+            headers: headers,
+            redirect: 'follow'
+        }    
+        const getQuestionsRes = await fetch(`https://iad1.qualtrics.com/API/v3/survey-definitions/${ids.SURVEY_ID}/questions`, getQuestionsDataRequestOptions)
+        const getQuestionsObj = await getQuestionsRes.json()
+        const questionMap = {}
+        getQuestionsObj.result.elements.forEach((e)=>{
+            questionMap[e.QuestionID] = e
+        })
+        console.log(questionMap)
+    
+        // starting the response export
         const startExportData = JSON.stringify({
             "format": "json",
             "compress" : false
         })
-
         const startExportRequestOptions = {
             method: 'POST',
             headers,
             body: startExportData,
             redirect: 'follow'
         }
-      
         const startExportRes = await fetch(`https://iad1.qualtrics.com/API/v3/surveys/${ids.SURVEY_ID}/export-responses`, startExportRequestOptions)
         const startExportObj = await startExportRes.json()
+
 
         //Get the progress of the export
         const getExportProgressRequestOptions = {
@@ -28,8 +41,17 @@ const ExportData = () => {
             redirect: 'follow'
         }
       
-        const getExportProgressRes = await fetch(`https://iad1.qualtrics.com/API/v3/surveys/${ids.SURVEY_ID}/export-responses/${startExportObj.result.progressId}`, getExportProgressRequestOptions)
-        const getExportProgressObj = await getExportProgressRes.json()
+        let getExportProgressRes = await fetch(`https://iad1.qualtrics.com/API/v3/surveys/${ids.SURVEY_ID}/export-responses/${startExportObj.result.progressId}`, getExportProgressRequestOptions)
+        let getExportProgressObj = await getExportProgressRes.json()
+        console.log("Progress")
+        console.log(getExportProgressObj)
+        if(getExportProgressObj.result.status != "complete") {
+            // TODO: await a couple seconds
+            getExportProgressRes = await fetch(`https://iad1.qualtrics.com/API/v3/surveys/${ids.SURVEY_ID}/export-responses/${startExportObj.result.progressId}`, getExportProgressRequestOptions)
+            getExportProgressObj = await getExportProgressRes.json()
+            console.log("Progress")
+            console.log(getExportProgressObj)
+        }
 
 
         //Get the export file
@@ -37,13 +59,50 @@ const ExportData = () => {
             method: 'GET',
             headers,
             redirect: 'follow'
-        }
-      
+        }      
         const getExportFileRes = await fetch(`https://iad1.qualtrics.com/API/v3/surveys/${ids.SURVEY_ID}/export-responses/${getExportProgressObj.result.fileId}/file`, getExportFileRequestOptions)
         const getExportFileObj = await getExportFileRes.json()
-
         console.log('export file obj ', getExportFileObj)
 
+        // Transform file
+        let results = []
+        getExportFileObj.responses.forEach((response)=>{
+            console.log(response)
+            if(response.values.finished!=1) {
+                console.log("ignoring")
+                return // ignore unfinished respones
+            }
+
+            let values = response.values
+            let row = {items:{}}
+
+            Object.keys(values).forEach((key)=>{
+                let qid = key.split("_")[0]                
+                if(questionMap[qid]) {
+                    let sfidparts = questionMap[qid].DataExportTag.split("_")
+                    let sfid = sfidparts[0] // first part is the sf ID
+                    let isText = (sfidparts.length==2) // if second part exists, then it's the text part of the question
+                    if(isText){
+                        if(!row.items[sfid]) row.items[sfid]={}
+                        row.items[sfid].text = values[key]
+                    } else {
+                        if(!row.items[sfid]) row.items[sfid]={}
+                        row.items[sfid].sfid = sfid
+                        row.items[sfid].value = values[key]
+                        row.items[sfid].label = questionMap[qid].Choices[values[key]].Display
+                    }
+                } else {
+                    let metaList = ["externalDataReference","recipientEmail","recipientFirstName","recipientLastName","recordedDate","responseId"]
+                    if(metaList.includes(key)) {
+                        row[key] = values[key]
+                    }
+                }
+
+            })
+            results.push(row)
+        })
+
+        console.log("Transformed data",results)
     }
 
     return (
