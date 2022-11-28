37e95a52ed7159eaa65222f152ccfebeb5687bb4
Chris Steele <chris@me.com>
2022-11-28 13:17:40 -0600

    added reorder test button

---
+++ src/Components/ReorderQuestions.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import { reorderQuestions } from '../API/qualtricsAPI'
+
+const ReorderQuestions = ()=> {
+    const [questionIDs, setQuestionIDs] = useState('')
+
+
+    return (
+        <div>
+            <div style={{display:"flex", flexDirection: "column"}}>
+            <label>questionIDs</label>
+            <textarea value={questionIDs} onChange={(e)=>{setQuestionIDs(e.target.value)}}/>
+            </div>
+            <button onClick={()=>{reorderQuestions({ order:questionIDs.split(",") })}} className='Button'>
+               reorderQuestion
+            </button>
+        </div>
+    )
+}
+
+export default ReorderQuestions
